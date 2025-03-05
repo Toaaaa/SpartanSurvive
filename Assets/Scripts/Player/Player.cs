14bb84019fb188d542811c290c69fac4dbe153ee
kim minjun <k980128@naver.com>
2025-03-05 21:55:15 +0900

    feat : Player hp access level change.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] public float hp { get; set; } = 100;
-    [SerializeField] public float maxHp { get; set; } = 100;
+    [SerializeField] private float _hp = 100;// 직렬화 필드로 선언해 인스펙터에서 값을 변경할 수 있게 함.
+    public float hp
+    {
+        get => _hp;
+        private set => _hp = Mathf.Clamp(value, 0, maxHp); // 체력의 상한치를 maxHp로 제한.
+    }
+    private float maxHp = 100;
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+    }
+    public void Heal(float heal)
+    {
+        hp += heal;
+    }
+    public float GetMaxHp()
+    {
+        return maxHp;
+    }
 }
