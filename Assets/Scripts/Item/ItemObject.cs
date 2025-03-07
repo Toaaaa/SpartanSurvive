69de9d2f481ac09f391bef54526f7bbd8202eb46
kim minjun <k980128@naver.com>
2025-03-07 15:52:05 +0900

    feat : Dotween yoyo for Item objects.

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemObject : MonoBehaviour
 {
     public ItemData itemData;// 해당 아이템 게임 오브젝트의 아이템 데이터.
+
+    private void Start()
+    {
+        UpdownMove();
+    }
+
+    void UpdownMove()
+    {
+        transform.DOLocalMoveY(1.2f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
 }
